8f621bc73287f88251de2fc98e1ae9d0050b432c
smita patel <smita.mgs1215@gmail.com>
2025-03-01 11:18:46 +0530

    current changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -22,9 +22,9 @@ app.use(cookieParser());
 app.use(
     cors({
         origin: "http://localhost:5173",
-        credentials: true
+        credentials: true,
     })
-)
+);
 
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
